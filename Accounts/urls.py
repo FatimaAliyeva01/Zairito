57ad49388869c94361dc5a28722a7de42e3473e3
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-02-03 23:56:36 +0400

    createsuperuser

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from .views import Accounts
-# Create your views here.
-app_name='Accounts'
-urlpatterns = [
-    path('/',Accounts, name='accounts'),
-]
+# from django.urls import path
+# from .views import Accounts
+# # Create your views here.
+# app_name='Accounts'
+# urlpatterns = [
+#     path('/',Accounts, name='accounts'),
+# ]
 
 
