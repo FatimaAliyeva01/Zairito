58bed4c0f3a39f81ce56679222c07d7c8cb2f2e8
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-02-22 00:01:26 +0400

    Redis

--- zarito_stories/settings.py
+++ zarito_stories/settings.py
@@ -154,3 +154,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Baku'
+
+SITE_ADDRESS = 'http://localhost:8000'
