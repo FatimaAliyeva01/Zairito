d759e6b2942267cbacfc20a884ecc13df20b00a5
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-02-22 23:59:33 +0400

    Home app

--- Home/urls.py
+++ Home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import index
 from .views import index2
 from .views import index3
+from Home import views
 app_name='Home'
 urlpatterns = [
     path('', index, name='index'),
