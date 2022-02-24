fdfd236e3d54dbf2a9cf0f2a7e171b6ebb4d6f46
FatimaAliyeva01 <81676388+FatimaAliyeva01@users.noreply.github.com>
2022-02-24 20:54:56 +0400

    Index2.html head silindi

--- Home/models.py
+++ Home/models.py
@@ -1,7 +1,7 @@
-from django.db import models
-from sqlalchemy import null
+# from django.db import models
+# from sqlalchemy import null
 
-class About(models.Model):
-    image = models.ImageField(upload_to='about/', null=True, blank=True)
-    slogan = models.TextField(default="Innovative Solutions To Boost Your Projects estibulum ante ipsum primis in faucibus orci luctus et ultrices posuere consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec,")
+# class About(models.Model):
+#     image = models.ImageField(upload_to='about/', null=True, blank=True)
+#     slogan = models.TextField(default="Innovative Solutions To Boost Your Projects estibulum ante ipsum primis in faucibus orci luctus et ultrices posuere consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec,")
     
